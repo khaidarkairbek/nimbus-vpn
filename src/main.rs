6a0b54b49401579fd9561379700ef574620d18bc
Work <khaidarkairbek@proton.me>
2024-06-24 19:17:29 -0400

    init: socket and utun poll

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,52 @@
+use mio::{Events, Poll as Mio_Poll, Interest, Token};
+use mio::unix::SourceFd;
+use tun::TunDevice;
+use mio::net::UdpSocket;
+use std::os::fd::AsRawFd; 
+
 mod tun;
 
+pub struct Executor {
+    pub clients: Vec<UdpSocket>, 
+    pub poll: Mio_Poll, 
+    pub events: Events,
+}
+
 fn main() {
+
+    let mut poll = Mio_Poll::new().unwrap();
+
+    let mut events = Events::with_capacity(1024); 
+
+    let mut sock_fd = UdpSocket::bind("127.0.0.1:0".parse().unwrap()).unwrap();
+
+    let mut tun = TunDevice::create(100).unwrap();
+    tun.up();
+    let mut tun_raw_fd = tun.file.as_raw_fd(); 
+
+    let mut tun_fd = SourceFd(&mut tun_raw_fd); 
+
+    poll.registry().register(&mut sock_fd, Token(1), Interest::READABLE).unwrap();
+
+    poll.registry().register(&mut tun_fd, Token(2), Interest::READABLE).unwrap();
+
+    loop {
+        poll.poll(&mut events, None).unwrap();
+        for event in &events {
+            match event.token() {
+                Token(1) => {
+                    let mut buffer = [0; 2000]; 
+                    let (len, from_addr) = sock_fd.recv_from(&mut buffer).unwrap();
+                    println!("Socket received {:?} from {:?}", &buffer[..len], from_addr); 
+                }, 
+                Token(2) => {
+                    let mut buffer = [0; 2000]; 
+                    let len = tun.read(&mut buffer).unwrap();
+                    println!("TUN received {:?}", &buffer[..len]); 
+                },
+                Token(_) => (),
+            }
+        }
+    }
 
 }
--- src/tun.rs
+++ src/tun.rs
@@ -30,103 +30,113 @@ pub struct TunDevice {
 }
 
 const MTU: &'static str = "1380"; 
+impl TunDevice {
+    // Creates the TUN device of specified number
+    pub fn create(tun_num: u8) -> Result<TunDevice, Error>{
 
-// Creates the TUN device of specified number
-pub fn create(tun_num: &u8) -> Result<File, Error>{
+        const DOMAIN: i32 = libc::PF_SYSTEM; // Protocol domain to be used
+        const TY: i32 = libc::SOCK_DGRAM;   // Communication semantics
+        const PROTOCOL: i32 = libc::SYSPROTO_CONTROL;  // Protocol to be used for the socket
 
-    const DOMAIN: i32 = libc::PF_SYSTEM; // Protocol domain to be used
-    const TY: i32 = libc::SOCK_DGRAM;   // Communication semantics
-    const PROTOCOL: i32 = libc::SYSPROTO_CONTROL;  // Protocol to be used for the socket
+        
+        // Create a socket (endpoint for communication)
+        let sock_fd = unsafe {libc::socket(DOMAIN, TY, PROTOCOL)};
 
-    
-    // Create a socket (endpoint for communication)
-    let sock_fd = unsafe {libc::socket(DOMAIN, TY, PROTOCOL)};
+        if sock_fd == -1 {
+            eprintln!("Problem opening a socket"); 
+            return Err(Error::last_os_error());
+        };
 
-    if sock_fd == -1 {
-        eprintln!("Problem opening a socket"); 
-        return Err(Error::last_os_error());
-    };
+        let file = unsafe{std::fs::File::from_raw_fd(sock_fd)};
 
-    let file = unsafe{std::fs::File::from_raw_fd(sock_fd)};
+        // Kernel Control Info
+        let mut ctl_info: CtlInfo = CtlInfo {
+            ctl_id : 0, 
+            ctl_name : [0u8; 96]
+        };
 
-    // Kernel Control Info
-    let mut ctl_info: CtlInfo = CtlInfo {
-        ctl_id : 0, 
-        ctl_name : [0u8; 96]
-    };
+        unsafe {
+            libc::memset(&mut ctl_info as *mut _ as *mut libc::c_void, 0, mem::size_of::<CtlInfo>() as libc::size_t); // Clear the kernel info by modifying the memory
+            let ctl_name = std::ffi::CString::new("com.apple.net.utun_control").unwrap();
+            libc::strncpy(ctl_info.ctl_name.as_mut_ptr() as *mut libc::c_char, ctl_name.as_ptr(), 96);  // Set the kernel control name
+        }; 
 
-    unsafe {
-        libc::memset(&mut ctl_info as *mut _ as *mut libc::c_void, 0, mem::size_of::<CtlInfo>() as libc::size_t); // Clear the kernel info by modifying the memory
-        let ctl_name = std::ffi::CString::new("com.apple.net.utun_control").unwrap();
-        libc::strncpy(ctl_info.ctl_name.as_mut_ptr() as *mut libc::c_char, ctl_name.as_ptr(), 96);  // Set the kernel control name
-    }; 
+        if unsafe {libc::ioctl(file.as_raw_fd(), libc::CTLIOCGINFO, &mut ctl_info)} == -1 {  // Getting kernel control id, break if not succesfull
+            eprintln!("Problem getting kernel control id");
+            return Err(Error::last_os_error());
+        };
 
-    if unsafe {libc::ioctl(file.as_raw_fd(), libc::CTLIOCGINFO, &mut ctl_info)} == -1 {  // Getting kernel control id, break if not succesfull
-        eprintln!("Problem getting kernel control id");
-        return Err(Error::last_os_error());
-    };
+        // Instantiate the kernel control connection data
+        let sockaddr_ctl: SockAddrCtl = SockAddrCtl {   
+            sc_len : mem::size_of::<SockAddrCtl>() as u8, 
+            sc_family : DOMAIN as u8,
+            ss_sysaddr : libc::AF_SYS_CONTROL as u16,
+            sc_unit : {tun_num + 1}  as u32, 
+            sc_id : ctl_info.ctl_id,
+            sc_reserved : [0; 5]
+        };
 
-    // Instantiate the kernel control connection data
-    let sockaddr_ctl: SockAddrCtl = SockAddrCtl {   
-        sc_len : mem::size_of::<SockAddrCtl>() as u8, 
-        sc_family : DOMAIN as u8,
-        ss_sysaddr : libc::AF_SYS_CONTROL as u16,
-        sc_unit : {tun_num + 1}  as u32, 
-        sc_id : ctl_info.ctl_id,
-        sc_reserved : [0; 5]
-    };
+        // Connect to the kernel control
+        if unsafe {libc::connect(file.as_raw_fd(), &sockaddr_ctl as *const _ as *const libc::sockaddr, mem::size_of_val(&sockaddr_ctl) as u32)} == -1 {
+            eprintln!("Problem connecting to the socket");
+            return Err(Error::last_os_error());
+        }
 
-    // Connect to the kernel control
-    if unsafe {libc::connect(file.as_raw_fd(), &sockaddr_ctl as *const _ as *const libc::sockaddr, mem::size_of_val(&sockaddr_ctl) as u32)} == -1 {
-        eprintln!("Problem connecting to the socket");
-        return Err(Error::last_os_error());
+        if unsafe {libc::fcntl(file.as_raw_fd(), libc::F_SETFL, libc::O_NONBLOCK)} == -1 {
+            eprintln!("Problem setting the file descriptor to non-blocking mode");
+            return Err(Error::last_os_error());
+        }
+
+        println!("Connected and finished");
+        Ok(TunDevice {
+            file : file, 
+            id : tun_num
+        })
+
     }
 
-    println!("Connected and finished");
-    Ok(file)
-}
+    // Brings the tun device up and sets it up using ifconfig to given ip address and MTU constant
+    pub fn up(&self) {
 
-// Brings the tun device up and sets it up using ifconfig to given ip address and MTU constant
-pub fn up(tun : &TunDevice) {
+        //  Assign IP addresses to TUN and bring it up
+        let mut status = process::Command::new("ifconfig").arg(format!("utun{}", self.id)).arg("69.69.69.11").arg("69.69.69.12").status().unwrap(); 
+        assert!(status.success()); 
 
-    //  Assign IP addresses to TUN and bring it up
-    let mut status = process::Command::new("ifconfig").arg(format!("utun{}", tun.id)).arg("69.69.69.11").arg("69.69.69.12").status().unwrap(); 
-    assert!(status.success()); 
-
-    // Setting MTU (Maximum Transmission Unit) value to 1380, which is a standard for VPN application
-    status = process::Command::new("ifconfig").arg(format!("utun{}", tun.id)).arg("mtu").arg(MTU).status().unwrap();
-    assert!(status.success());
-}
-
-
-// Write the data into tun device from the buffer, returns the length of the written data or Error
-pub fn write(tun : &mut TunDevice, buffer : &[u8]) -> Result<usize, std::io::Error>{
-
-    let mut packet = match buffer[0] & 0xf {
-        6 => vec![0, 0, 0, 10],   // 4 byte header for ipv6 packet on MacOS
-        _ => vec![0, 0, 0, 2]     // 4 byte header for ipv4 packet on MacOS
-    }; 
-    
-    packet.write_all(buffer)?;
-
-    match tun.file.write(&packet) {
-        Ok(len) => if len > 4 { Ok(len - 4) } else { Ok(0) }, 
-        Err(e) => Err(e)
+        // Setting MTU (Maximum Transmission Unit) value to 1380, which is a standard for VPN application
+        status = process::Command::new("ifconfig").arg(format!("utun{}", self.id)).arg("mtu").arg(MTU).status().unwrap();
+        assert!(status.success());
     }
-}
 
 
-// Read the data from tun device into the buffer, returns the length of the read data or Error
-pub fn read(tun : &mut TunDevice, buffer : &mut [u8]) -> Result<usize, std::io::Error> {
+    // Write the data into tun device from the buffer, returns the length of the written data or Error
+    pub fn write(&mut self, buffer : &[u8]) -> Result<usize, std::io::Error>{
 
-    let mut packet = [0; 2000];  // the regular MTU is about 1500, so 2000 should be sufficient
+        let mut packet = match buffer[0] & 0xf {
+            6 => vec![0, 0, 0, 10],   // 4 byte header for ipv6 packet on MacOS
+            _ => vec![0, 0, 0, 2]     // 4 byte header for ipv4 packet on MacOS
+        }; 
+        
+        packet.write_all(buffer)?;
 
-    match tun.file.read(&mut packet) {
-        Ok(len) => {
-            let data = &packet[4..len];   // removing the 4 byte header, signifying ipv6 or ipv4 packet
-            buffer[..len-4].clone_from_slice(data);  // writing the data into the beginning of the buffer
-            if len > 4 { Ok(len - 4) } else { Ok(0) }}  
-        Err(e) => Err(e)
+        match self.file.write(&packet) {
+            Ok(len) => if len > 4 { Ok(len - 4) } else { Ok(0) }, 
+            Err(e) => Err(e)
+        }
+    }
+
+
+    // Read the data from tun device into the buffer, returns the length of the read data or Error
+    pub fn read(&mut self, buffer : &mut [u8]) -> Result<usize, std::io::Error> {
+
+        let mut packet = [0; 2000];  // the regular MTU is about 1500, so 2000 should be sufficient
+
+        match self.file.read(&mut packet) {
+            Ok(len) => {
+                let data = &packet[4..len];   // removing the 4 byte header, signifying ipv6 or ipv4 packet
+                buffer[..len-4].clone_from_slice(data);  // writing the data into the beginning of the buffer
+                if len > 4 { Ok(len - 4) } else { Ok(0) }}  
+            Err(e) => Err(e)
+        }
     }
 }
 
@@ -144,13 +154,13 @@ mod tests {
                 panic!("No available space for new tun device!")
             }
     
-            match create(&tun_num) {
-                Ok(tun_file) =>  break TunDevice{file : tun_file, id : tun_num},
+            match TunDevice::create(tun_num) {
+                Ok(tun) =>  break tun,
                 Err(_) => tun_num += 1,
             }
         };
     
-        up(&tun);
+        tun.up();
     
         println!("Created tun device: {:?}", tun);
 
