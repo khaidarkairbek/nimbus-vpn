dc69692232cf2e6b58e4cee0aa744a57f283363b
Work <khaidarkairbek@proton.me>
2024-07-16 20:42:52 -0400

    changed error messages a little

--- src/error.rs
+++ src/error.rs
@@ -3,78 +3,78 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum TunInitError {
-    #[error("Problem opening a socket")]
+    #[error("Problem opening a socket.")]
     TunSocketOpenError(#[from] io::Error), 
-    #[error("Problem getting kernel control id")]
+    #[error("Problem getting kernel control ID.")]
     KernelCtrlIdError(io::Error), 
-    #[error("Problem connecting to the tun's socket")]
+    #[error("Problem connecting to the TUN's socket.")]
     TunSocketConnectError(io::Error), 
-    #[error("Problem setting TUN to non-blocking mode")]
+    #[error("Problem setting TUN to non-blocking mode.")]
     NonBlockError(io::Error),
 }
 
 #[derive(Error, Debug)]
 pub enum TunOperationError {
-    #[error("Problem writing to tun device")]
+    #[error("Problem writing to TUN device.")]
     TunWriteError(io::Error),
-    #[error("Problem reading tun device")]
+    #[error("Problem reading TUN device.")]
     TunReadError(io::Error), 
 }
 
 #[derive(Error, Debug)]
 pub enum ServerError {
-    #[error("Client address is not specified for a key")]
+    #[error("Client address is not specified for a key.")]
     ClientInfoSetError, 
-    #[error("Client address is not specified for a key")]
+    #[error("Client address is not specified for a key.")]
     ClientInfoGetError,
-    #[error("Client info not found in the map")]
+    #[error("Client info not found in the map.")]
     ClientInfoNotFound, 
-    #[error("No available port for new connection")]
+    #[error("No available port for new connection.")]
     ServerPortError,
 }
 
 #[derive(Error, Debug)]
 pub enum ClientError {
-    #[error("Shared secret key not specified for a client")]
+    #[error("Shared secret key not specified for a client.")]
     SharedKeyGetError, 
 }
 
 #[derive(Error, Debug)]
 pub enum SocketError {
-    #[error("Socket send error")]
+    #[error("Socket send error.")]
     SocketSendToError(String), 
-    #[error("Socket read error")]
+    #[error("Socket read error.")]
     SocketReadError(String), 
-    #[error("Socket binding error")]
+    #[error("Socket binding error.")]
     SocketBindError(String)
 }
 
 #[derive(Error, Debug)]
 pub enum LogicError {
-    #[error("Not expected behaviour")]
+    #[error("Unexpected behavior.")]
     IncorrectRecepientError, 
-    #[error("Incorrect message type")]
+    #[error("Incorrect message type.")]
     IncorrectMessageError
 }
 
 #[derive(Error, Debug)]
 pub enum CommError {
-    #[error("Mio Poll registry error")]
+    #[error("Mio Poll registry error.")]
     MioRegistryError, 
-    #[error("Mio Poll polling error")]
+    #[error("Mio Poll polling error.")]
     MioPollingError, 
-    #[error("Mio Poll initiation error")]
+    #[error("Mio Poll initiation error.")]
     MioInitError, 
-    #[error("Deserialization problem")]
+    #[error("Deserialization problem.")]
     DeserialError(String), 
-    #[error("Serialization problem")]
+    #[error("Serialization problem.")]
     SerialError(String), 
 }
 
 #[derive(Error, Debug)]
 pub enum CryptoError {
-    #[error("Encryption Failed")]
+    #[error("Encryption failed.")]
     EncryptError(String), 
-    #[error("Decryption Failed")]
+    #[error("Decryption failed.")]
     DecryptError(String),
 }
